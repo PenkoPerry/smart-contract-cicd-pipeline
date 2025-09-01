131453ec39148ed16657beebb7214bda3b4888b3
Kaushal Waray <kaushalwaray@gmail.com>
2025-09-01 15:50:15 +0530

    Update Lock.test.js

--- test/Lock.test.js
+++ test/Lock.test.js
@@ -1,1 +1,59 @@
+const {
+  time,
+  loadFixture,
+} = require("@nomicfoundation/hardhat-toolbox/network-helpers");
+const { expect } = require("chai");
 
+// These are the unit tests for the Lock.sol contract.
+// The CI/CD pipeline will run these tests automatically.
+describe("Lock", function () {
+  // We define a fixture to reuse the same setup in every test.
+  async function deployOneYearLockFixture() {
+    const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
+    const ONE_GWEI = 1_000_000_000;
+
+    const lockedAmount = ONE_GWEI;
+    const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
+
+    const [owner, otherAccount] = await ethers.getSigners();
+
+    const Lock = await ethers.getContractFactory("Lock");
+    const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+
+    return { lock, unlockTime, lockedAmount, owner, otherAccount };
+  }
+
+  describe("Deployment", function () {
+    it("Should set the right unlockTime", async function () {
+      const { lock, unlockTime } = await loadFixture(deployOneYearLockFixture);
+      expect(await lock.unlockTime()).to.equal(unlockTime);
+    });
+
+    it("Should set the right owner", async function () {
+      const { lock, owner } = await loadFixture(deployOneYearLockFixture);
+      expect(await lock.owner()).to.equal(owner.address);
+    });
+  });
+
+  describe("Withdrawals", function () {
+    it("Should revert with the right error if called too soon", async function () {
+      const { lock } = await loadFixture(deployOneYearLockFixture);
+      await expect(lock.withdraw()).to.be.revertedWith(
+        "You can't withdraw yet"
+      );
+    });
+
+    it("Should transfer the funds to the owner after unlock time", async function () {
+        const { lock, unlockTime, owner, lockedAmount } = await loadFixture(
+          deployOneYearLockFixture
+        );
+  
+        await time.increaseTo(unlockTime);
+  
+        await expect(lock.withdraw()).to.changeEtherBalances(
+          [owner, lock],
+          [lockedAmount, -lockedAmount]
+        );
+      });
+  });
+});
